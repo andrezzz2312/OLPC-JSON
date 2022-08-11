a6557baa8691eaed21cd477c7bb4e69ef188f1d3
Andrez Ramirez <andrezzz2312@gmail.com>
2022-08-11 11:27:48 -0500

    final

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'src'),
+		},
+		compress: true,
+		port: 5000,
+	},
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	mode: 'development',
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Galaxy',
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: 'src/css/index.css',
+					to: '',
+				},
+				{
+					from: 'src/ass',
+					to: 'assets',
+				},
+				{
+					from: 'src/assets',
+					to: 'assets',
+				},
+				{
+					from: 'src/assets',
+					to: 'assets',
+				},
+			],
+		}),
+	],
+}
